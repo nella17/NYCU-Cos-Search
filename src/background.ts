4008fb7d6c934d55bd8996510b9321182590934c
Allen Guan <me@nella17.tw>
2023-01-21 23:11:15 +0800

    get token from webRequest

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,27 @@
+let token = null as string | null
+
 chrome.action.onClicked.addListener((tab) => {
     if (tab.id) {
         chrome.tabs.sendMessage(tab.id, { toggleVisible: true })
+    }
+})
+
+chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        if (token) return
+        const data = details.requestBody?.formData
+        if (!data) return
+        token = data.token[0]
+    },
+    {
+        urls: ['https://cos.nycu.edu.tw/*'],
+    },
+    ['requestBody'],
+)
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'getToken') {
+        sendResponse({ token })
+        token = null
     }
 })
--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -11,3 +11,23 @@ chrome.runtime.onMessage.addListener((message) => {
         ;(vm.$props as any).visible = !(vm.$props as any).visible
     }
 })
+
+function get(path: string, body: Record<string, string>) {
+    return fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(body),
+    })
+}
+
+chrome.runtime.sendMessage({ action: 'getToken' }).then(async ({ token }) => {
+    get('/sysstatuslvl', { token })
+        .then((resp) => resp.json())
+        .then((resp) => console.log(resp))
+
+    get('/getpreregist', { token })
+        .then((resp) => resp.json())
+        .then((resp) => console.log(resp))
+})
