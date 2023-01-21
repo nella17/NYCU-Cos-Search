338d2e579f621a33ab7dc8c7509a36d24ed31802
Allen Guan <me@nella17.tw>
2023-01-22 00:16:59 +0800

    save last visible

--- src/content-scripts/main.vue
+++ src/content-scripts/main.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
-const visible = ref(true)
+const visible = ref(false)
+
+chrome.storage.local.get(['visible']).then((result) => {
+    visible.value = result.visible
+})
 
 function toggleVisible() {
     visible.value = !visible.value
 }
+
+watch(visible, (value) => {
+    chrome.storage.local.set({ visible: value })
+})
 
 chrome.runtime.onMessage.addListener((message) => {
     if (message.toggleVisible) toggleVisible()
